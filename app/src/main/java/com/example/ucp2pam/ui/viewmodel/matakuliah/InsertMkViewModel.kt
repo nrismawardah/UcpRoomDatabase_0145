d07178be7e449db533d1a19563afdcc53d98bc1f
nrismawardah <nrismawardah@gmail.com>
2024-12-20 01:51:13 +0700

    Menambahkan function MatakuliahEvent.toMatakuliahEntity

--- app/src/main/java/com/example/ucp2pam/ui/viewmodel/matakuliah/InsertMkViewModel.kt
+++ app/src/main/java/com/example/ucp2pam/ui/viewmodel/matakuliah/InsertMkViewModel.kt
@@ -1,5 +1,16 @@
 package com.example.ucp2pam.ui.viewmodel.matakuliah
 
+import com.example.ucp2pam.data.entity.Matakuliah
+
+fun MatakuliahEvent.toMatakuliahEntity(): Matakuliah = Matakuliah(
+    kodeMK = kodeMK,
+    namaMK = namaMK,
+    sksMK = sksMK,
+    semesterMK = semesterMK,
+    jenisMK = jenisMK,
+    dosenMK = dosenMK
+)
+
 data class MatakuliahEvent(
     val kodeMK: String = "",
     val namaMK: String = "",
