67cade2d170338ad67a5db5dae427f1ddd7a34be
nrismawardah <nrismawardah@gmail.com>
2024-12-19 23:40:47 +0700

    Menambahkan function DosenEvent.toDosenEntity

--- app/src/main/java/com/example/ucp2pam/ui/viewmodel/dosen/InsertDosenViewModel.kt
+++ app/src/main/java/com/example/ucp2pam/ui/viewmodel/dosen/InsertDosenViewModel.kt
@@ -1,5 +1,13 @@
 package com.example.ucp2pam.ui.viewmodel.dosen
 
+import com.example.ucp2pam.data.entity.Dosen
+
+fun DosenEvent.toDosenEntity(): Dosen = Dosen(
+    nidn = nidn,
+    nama = nama,
+    jenisKelamin = jenisKelamin
+)
+
 data class DosenEvent(
     val nidn: String = "",
     val nama: String = "",
