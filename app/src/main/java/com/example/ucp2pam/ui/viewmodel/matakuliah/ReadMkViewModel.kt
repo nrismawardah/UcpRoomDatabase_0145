55c1bb7e0bbf86f610b53b8acd5d685fa6d7f22d
nrismawardah <nrismawardah@gmail.com>
2024-12-20 01:39:53 +0700

    Ganti nama untuk data class ReadMkViewModel

--- app/src/main/java/com/example/ucp2pam/ui/viewmodel/matakuliah/ReadMkViewModel.kt
+++ app/src/main/java/com/example/ucp2pam/ui/viewmodel/matakuliah/ReadMkViewModel.kt
@@ -1,8 +1,15 @@
 package com.example.ucp2pam.ui.viewmodel.matakuliah
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ucp2pam.data.entity.Matakuliah
+import com.example.ucp2pam.repository.RepositoyMk
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
 
-data class ReadMkViewModel (
+
+data class ReadMkUiState (
     val listMk: List<Matakuliah> = listOf(),
     val isLoading: Boolean = false,
     val isError: Boolean = false,
