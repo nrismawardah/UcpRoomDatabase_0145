3d1d1442e54711ef5d2bc79528f9c618dd9fd5fa
nrismawardah <nrismawardah@gmail.com>
2024-12-20 01:46:32 +0700

    Membuat view model matakulih pada operasi read

--- app/src/main/java/com/example/ucp2pam/ui/viewmodel/matakuliah/ReadMkViewModel.kt
+++ app/src/main/java/com/example/ucp2pam/ui/viewmodel/matakuliah/ReadMkViewModel.kt
@@ -1,6 +1,51 @@
 package com.example.ucp2pam.ui.viewmodel.matakuliah
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ucp2pam.data.entity.Matakuliah
+import com.example.ucp2pam.repository.RepositoryMk
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+
+class ReadMkViewModel(
+    private val repositoryMk: RepositoryMk
+) : ViewModel() {
+
+    val readMkUiState: StateFlow<ReadMkUiState> = repositoryMk.getAllMk()
+        .filterNotNull()
+        .map {
+            ReadMkUiState(
+                listMk = it.toList(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(ReadMkUiState(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                ReadMkUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = ReadMkUiState(
+                isLoading = true,
+            )
+        )
+}
 
 data class ReadMkUiState (
     val listMk: List<Matakuliah> = listOf(),
