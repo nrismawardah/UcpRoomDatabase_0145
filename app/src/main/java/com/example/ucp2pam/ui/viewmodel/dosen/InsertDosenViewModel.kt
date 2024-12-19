48232f255501222a21b22dd71a901d6e14bf585f
nrismawardah <nrismawardah@gmail.com>
2024-12-19 23:43:25 +0700

    Membuat data class FormErrorStateDosenuntuk view model dosen pada operasi insert

--- app/src/main/java/com/example/ucp2pam/ui/viewmodel/dosen/InsertDosenViewModel.kt
+++ app/src/main/java/com/example/ucp2pam/ui/viewmodel/dosen/InsertDosenViewModel.kt
@@ -2,6 +2,22 @@ package com.example.ucp2pam.ui.viewmodel.dosen
 
 import com.example.ucp2pam.data.entity.Dosen
 
+data class DosenUiState(
+    val dosenEvent: DosenEvent = DosenEvent(),
+    val isEntryValid: FormErrorStateDosen = FormErrorStateDosen(),
+    val snackBarMessage: String? = null
+)
+
+data class FormErrorStateDosen(
+    val nidn: String? = null,
+    val nama: String? = null,
+    val jenisKelamin: String? = null,
+){
+    fun isValid(): Boolean{
+        return nidn == null && nama == null && jenisKelamin == null
+    }
+}
+
 fun DosenEvent.toDosenEntity(): Dosen = Dosen(
     nidn = nidn,
     nama = nama,
