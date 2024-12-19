20472047df79a9f831e85b557de8d19b95cbdcdd
nrismawardah <nrismawardah@gmail.com>
2024-12-20 01:49:16 +0700

    Membuat data class untuk view model matakuliah pada operasi insert

---
+++ app/src/main/java/com/example/ucp2pam/ui/viewmodel/matakuliah/InsertMkViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.ucp2pam.ui.viewmodel.matakuliah
+
+data class MatakuliahEvent(
+    val kodeMK: String = "",
+    val namaMK: String = "",
+    val sksMK: String = "",
+    val semesterMK: String = "",
+    val jenisMK: String = "",
+    val dosenMK: String = ""
+)
