304fd2a8a182316419a4dc9e741e0b1bb8000ed6
nrismawardah <nrismawardah@gmail.com>
2024-12-20 02:05:25 +0700

    Membuat data class DetailMkUiState pada view model matakuliah pada operasi detail

--- app/src/main/java/com/example/ucp2pam/ui/viewmodel/matakuliah/DetailMkViewModel.kt
+++ app/src/main/java/com/example/ucp2pam/ui/viewmodel/matakuliah/DetailMkViewModel.kt
@@ -2,6 +2,19 @@ package com.example.ucp2pam.ui.viewmodel.matakuliah
 
 import com.example.ucp2pam.data.entity.Matakuliah
 
+data class DetailMkUiState(
+    val detailMkUiEvent: MatakuliahEvent = MatakuliahEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+) {
+    val isUiEventEmpty: Boolean
+        get() = detailMkUiEvent == MatakuliahEvent()
+
+    val isUiEventNotEmpty: Boolean
+        get() = detailMkUiEvent != MatakuliahEvent()
+}
+
 fun Matakuliah.toDetailMkUiEvent() : MatakuliahEvent {
     return MatakuliahEvent(
         kodeMK = kodeMK,
