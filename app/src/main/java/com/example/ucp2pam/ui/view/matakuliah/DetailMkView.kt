aab3c7610f063658a97f7050651c12797d234856
nrismawardah <nrismawardah@gmail.com>
2024-12-21 04:36:45 +0700

    Membuat tampilan pada operasi Detail Matakuliah

--- app/src/main/java/com/example/ucp2pam/ui/view/matakuliah/DetailMkView.kt
+++ app/src/main/java/com/example/ucp2pam/ui/view/matakuliah/DetailMkView.kt
@@ -6,15 +6,21 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -25,9 +31,62 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.ucp2pam.data.entity.Matakuliah
+import com.example.ucp2pam.ui.customwidget.TopAppBar
+import com.example.ucp2pam.ui.viewmodel.PenyediaViewModel
 import com.example.ucp2pam.ui.viewmodel.matakuliah.DetailMkUiState
+import com.example.ucp2pam.ui.viewmodel.matakuliah.DetailMkViewModel
 import com.example.ucp2pam.ui.viewmodel.matakuliah.toMatakuliahEntity
+
+@Composable
+fun DetailMkView (
+    modifier: Modifier = Modifier,
+    viewModel: DetailMkViewModel = viewModel (factory = PenyediaViewModel.Factory),
+    onBack: () -> Unit = { },
+    onEditClick: (String) -> Unit = { },
+    onDeleteClick: () -> Unit = { }
+){
+    Scaffold (
+        modifier= Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+            .padding(top = 18.dp),
+        topBar = {
+            TopAppBar (
+                judul = "Detail Matakuliah",
+                showBackButton = true,
+                onBack = onBack,
+                modifier = modifier
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+                    onEditClick(viewModel.detailMkUiState.value.detailMkUiEvent.kodeMK)
+                },
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = "Edit Matakuliah",
+                )
+            }
+        }
+    ) { innerPadding ->
+        val detailUiState by viewModel.detailMkUiState.collectAsState()
+
+        BodyDetailMk(
+            modifier = Modifier.padding(innerPadding),
+            detailUiState = detailUiState,
+            onDeleteClick = {
+                viewModel.deleteMk()
+                onDeleteClick()
+            }
+        )
+    }
+}
 
 @Composable
 fun BodyDetailMk (
