20b4ceacba22531fd3da039fee929a1ab2fa22c0
nrismawardah <nrismawardah@gmail.com>
2024-12-21 02:48:22 +0700

    Membuat tampilan untuk Form Dosen pada operasi Insert Dosen

---
+++ app/src/main/java/com/example/ucp2pam/ui/view/dosen/InsertDosenView.kt
@@ -0,0 +1,91 @@
+package com.example.ucp2pam.ui.view.dosen
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import com.example.ucp2pam.ui.viewmodel.dosen.DosenEvent
+import com.example.ucp2pam.ui.viewmodel.dosen.FormErrorStateDosen
+
+@Composable
+fun FormDosen(
+    dosenEvent: DosenEvent = DosenEvent(),
+    onValueChange: (DosenEvent) -> Unit = {},
+    errorState: FormErrorStateDosen = FormErrorStateDosen(),
+    modifier: Modifier = Modifier
+){
+    val jenisKelamin = listOf("Laki-laki", "Perempuan")
+
+    Column (
+        modifier = modifier
+            .fillMaxWidth()
+    ){
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = dosenEvent.nidn,
+            onValueChange = {
+                onValueChange(dosenEvent.copy(nidn = it))
+            },
+            label = { Text("NIDN") },
+            isError = errorState.nidn !=null,
+            placeholder = { Text("Masukkan NIDN") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        )
+        Text(
+            text = errorState.nidn ?: "",
+            color = Color.Red
+        )
+        OutlinedTextField(
+            modifier = modifier.fillMaxWidth(),
+            value = dosenEvent.nama,
+            onValueChange = {
+                onValueChange(dosenEvent.copy(nama = it))
+            },
+            label = { Text("Nama") },
+            isError = errorState.nama !=null,
+            placeholder = { Text("Masukkan Nama") },
+        )
+        Text(
+            text = errorState.nama ?: "",
+            color = Color.Red
+        )
+        Spacer(
+            modifier = Modifier.height(5.dp)
+        )
+        Text(
+            text = "Jenis Kelamin"
+        )
+        Row (
+            modifier = Modifier.fillMaxWidth()
+        ){
+            jenisKelamin.forEach { jk ->
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Start
+                ){
+                    RadioButton(
+                        selected = dosenEvent.jenisKelamin == jk,
+                        onClick = {
+                            onValueChange(dosenEvent.copy(jenisKelamin = jk))
+                        },
+                    )
+                    Text(
+                        text = jk,
+                    )
+                }
+            }
+        }
+    }
+}
