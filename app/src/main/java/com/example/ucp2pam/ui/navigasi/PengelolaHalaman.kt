7f49d3c854434fd4dda81e92388d4185784c3e11
nrismawardah <nrismawardah@gmail.com>
2024-12-21 06:12:54 +0700

    Menambah navigasi pada PengelolaHalaman

--- app/src/main/java/com/example/ucp2pam/ui/navigasi/PengelolaHalaman.kt
+++ app/src/main/java/com/example/ucp2pam/ui/navigasi/PengelolaHalaman.kt
@@ -3,12 +3,18 @@ package com.example.ucp2pam.ui.navigasi
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.ucp2pam.ui.view.HomeScreen
+import com.example.ucp2pam.ui.view.dosen.InsertDosenView
 import com.example.ucp2pam.ui.view.dosen.ReadDosenView
+import com.example.ucp2pam.ui.view.matakuliah.DetailMkView
+import com.example.ucp2pam.ui.view.matakuliah.InsertMkView
 import com.example.ucp2pam.ui.view.matakuliah.ReadMkView
+import com.example.ucp2pam.ui.view.matakuliah.UpdateMkView
 
 @Composable
 fun PengelolaHalaman(
@@ -35,9 +41,11 @@ fun PengelolaHalaman(
             route = ReadDosen.route
         ){
             ReadDosenView(
-                onAddMDs = {},
-                onDetailClick = { nidn ->
-
+                onBack = {
+                    navController.navigate(Home.route)
+                },
+                onAddMDs = {
+                    navController.navigate(InsertDosen.route)
                 }
             )
         }
@@ -45,9 +53,83 @@ fun PengelolaHalaman(
             route = ReadMk.route
         ){
             ReadMkView(
-                onAddMk = {},
+                onBack = {
+                    navController.navigate(Home.route)
+                },
+                onAddMk = {
+                    navController.navigate(InsertMk.route)
+                },
                 onDetailClick = { kodeMK ->
                 }
+            )
+        }
+        composable(
+            route = InsertDosen.route
+        ){
+            InsertDosenView(
+                onBack = {
+                    navController.navigate(ReadDosen.route)
+                },
+                onNavigate = {
+                    navController.navigate(ReadDosen.route)
+                },
+                modifier = modifier
+            )
+        }
+        composable(
+            route = InsertMk.route
+        ){
+            InsertMkView(
+                onBack = {
+                    navController.navigate(ReadMk.route)
+                },
+                onNavigate = {
+                    navController.navigate(ReadMk.route)
+                },
+                modifier = modifier
+            )
+        }
+        composable(
+            DetailMk.routesWithArg,
+            arguments = listOf(
+                navArgument(DetailMk.KodeMk){
+                    type = NavType.StringType
+                }
+            )
+        ){
+            val kodeMk = it.arguments?.getString(DetailMk.KodeMk) ?: ""
+
+            kodeMk?.let { kodeMk ->
+                DetailMkView(
+                    onBack = {
+                        navController.popBackStack()
+                    },
+                    onEditClick = {
+                        navController.navigate("${UpdateMk.route}/$kodeMk")
+                    },
+                    modifier = modifier,
+                    onDeleteClick = {
+                        navController.popBackStack()
+                    }
+                )
+            }
+        }
+        composable(
+            UpdateMk.routesWithArg,
+            arguments = listOf(
+                navArgument(UpdateMk.KodeMk){
+                    type = NavType.StringType
+                }
+            )
+        ){
+            UpdateMkView(
+                onBack = {
+                    navController.popBackStack()
+                },
+                onNavigate = {
+                    navController.popBackStack()
+                },
+                modifier = modifier
             )
         }
     }
